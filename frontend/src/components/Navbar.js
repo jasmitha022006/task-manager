8fb2b4978e694ef5eda8d2d17ed8001c492806e2
Kesani Jasmitha <kesanijasmitha78@gmail.com>
2026-05-23 14:08:17 +0530

    restore frontend as normal react app

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Task Manager</h2>
+
+      <div>
+        <button className="nav-btn">Login</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
